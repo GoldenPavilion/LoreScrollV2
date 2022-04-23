a2e118ceba75f2b823bf591961ed20efd918ae77
GoldenPavilion <rcorystine@gmail.com>
2022-04-22 21:51:38 -0400

    created create action and user_params

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -9,4 +9,24 @@ class Api::UsersController < ApplicationController
         @user = User.find(params[:id])
         render json: User.new(@user)
     end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            render json: User.new(@user)
+        else
+            render json: {
+                error: "Username and E-mail must be unique."
+            }
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email, :penname)
+    end
+
 end
