b26fc71ef2c3fc18f989582f0de27e64d740eb91
GoldenPavilion <rcorystine@gmail.com>
2022-04-25 16:29:34 -0400

    seeded Users

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Leonardo", password: "password", email: "leonardo@tmnt.com", penname: "Leo Turtle");
+User.create(username: "Donatello", password: "password", email: "donatello@tmnt.com", penname: "Donny Turtle");
+User.create(username: "Michaelangelo", password: "password", email: "mikey@tmnt.com", penname: "Mikey Turtle");
+User.create(username: "Raphael", password: "password", email: "raphael@tmnt.com", penname: "Raffy Turtle");
