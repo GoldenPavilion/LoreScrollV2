c482625413375857461a8c44fe6fcedc07718778
GoldenPavilion <rcorystine@gmail.com>
2022-04-25 15:49:18 -0400

    added create action to CharactersController

--- backend/app/controllers/api/characters_controller.rb
+++ backend/app/controllers/api/characters_controller.rb
@@ -8,4 +8,14 @@ class Api::CharactersController < ApplicationController
         @character = Character.find(params[:id])
         render json: @character
     end
+
+    def create
+        @character = Character.new(character_params)
+
+        if @character.save
+            render json: @character, status: :created, location: @character
+        else
+            render json: @character.errors, status: :unprocessable_entity
+        end
+    end
 end
