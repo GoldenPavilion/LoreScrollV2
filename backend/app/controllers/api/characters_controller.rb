fc6de780db44013ff6adaa78e6b58b801218f724
GoldenPavilion <rcorystine@gmail.com>
2022-04-26 20:26:09 -0400

    updated CharactersController with serializers

--- backend/app/controllers/api/characters_controller.rb
+++ backend/app/controllers/api/characters_controller.rb
@@ -1,19 +1,19 @@
 class Api::CharactersController < ApplicationController
     def index
         @characters = Character.all
-        render json: @characters
+        render json: CharacterSerializer.new(@characters)
     end
 
     def show
         @character = Character.find(params[:id])
-        render json: @character
+        render json: CharacterSerializer.new(@character)
     end
 
     def create
         @character = Character.new(character_params)
 
         if @character.save
-            render json: @character, status: :created, location: @character
+            render json: CharacterSerializer.new(@character), status: :created, location: @character
         else
             render json: @character.errors, status: :unprocessable_entity
         end
