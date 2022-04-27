558c0cb274d44489037c0c162b378058fddd531f
GoldenPavilion <rcorystine@gmail.com>
2022-04-27 19:57:10 -0400

    added simple welcome

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Main = () => {
     return(
         <div className="main">
-            
+            <h1>Welcome to LoreScroll!</h1>
+            <h3>Your Storytelling Hub</h3>
         </div>
     )
 }
