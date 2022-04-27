779aaaa7d4022c2a9c563327053a822c7000894a
GoldenPavilion <rcorystine@gmail.com>
2022-04-26 20:27:08 -0400

    added setting attrs to serializer

--- backend/app/serializers/character_serializer.rb
+++ backend/app/serializers/character_serializer.rb
@@ -1,4 +1,4 @@
 class CharacterSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :role, :age, :gender, :species, :job, :physical, :personality, :history, :motivation
+  attributes :name, :role, :age, :gender, :species, :job, :physical, :personality, :history, :motivation, :setting
 end
