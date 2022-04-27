07b08c191b4cc574eac685910165133c2cd3f25e
GoldenPavilion <rcorystine@gmail.com>
2022-04-27 17:18:48 -0400

    imported necessary components from react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Main from './components/main/Main.js'
+import Main from './components/main/Main.js';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
 
 function App() {
   return (
