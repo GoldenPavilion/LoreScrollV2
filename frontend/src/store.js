049648ad25b74ff94eedc1a0cdb9c9a614b9a7a3
GoldenPavilion <rcorystine@gmail.com>
2022-04-28 22:40:06 -0400

    created composeEnhancer variable to connect to devtools

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,5 +2,8 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 const reducers = combineReducer({
-    
+
 })
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
