c3968ac164d3fe69a6a61b4a4f0d596fb8d41977
GoldenPavilion <rcorystine@gmail.com>
2022-05-02 21:36:00 -0400

    set up SET_CURRENT_USER case and default return

--- frontend/src/reducers/main/currentUserReducer.js
+++ frontend/src/reducers/main/currentUserReducer.js
@@ -1,6 +1,9 @@
 const currentUserReducer = (state = [], action) => {
     switch (action.type) {
-
+        case "SET_CURRENT_USER":
+            return action.user
+        default:
+            return state;
     }
 }
 
