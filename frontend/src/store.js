03591abd49947464107037a856b9515dec4401d5
GoldenPavilion <rcorystine@gmail.com>
2022-05-02 21:48:25 -0400

    fixed combineReducers bug

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 
 import currentUserReducer from './reducers/main/currentUserReducer'
 
-const reducers = combineReducer({
+const reducers = combineReducers({
     currentUserReducer
 })
 
