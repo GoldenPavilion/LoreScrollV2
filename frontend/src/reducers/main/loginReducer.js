6309fed2aa9fb3c6e5185ed1de908a68bf83edd3
GoldenPavilion <rcorystine@gmail.com>
2022-05-05 21:49:17 -0400

    added cases to loginReducer

--- frontend/src/reducers/main/loginReducer.js
+++ frontend/src/reducers/main/loginReducer.js
@@ -4,7 +4,14 @@ const initState = {
 }
 
 const loginReducer = (state = initState, action) => {
-
+    switch(action.type) {
+        case "UPDATE_LOGIN_FORM":
+            return action.formData;
+        case "RESET_LOGIN_FORM":
+            return initState;
+        default:
+            return state;
+    }
 }
 
 export default loginReducer;
