a5266d18f339f16d293da92b12cccff0870a617d
GoldenPavilion <rcorystine@gmail.com>
2022-05-09 23:40:39 -0400

    build create action for sessions

--- backend/app/controllers/api/sessions_controller.rb
+++ backend/app/controllers/api/sessions_controller.rb
@@ -1,5 +1,15 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by(username: params[:session][:username])
+    if @user && @user.authenticate(params[:session][:password])
+      sesson[:user_id] = @user.id
+      render json: @user
+    else
+      flash[:notify] = "Something went wrong. Try again!"
+      render json: {
+        error: "Invalid"
+      }
+    end
   end
 
   def destroy
