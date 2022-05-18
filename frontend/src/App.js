e9f6c981e6790e3929c7265804c8af9795a27251
GoldenPavilion <rcorystine@gmail.com>
2022-05-17 21:53:47 -0400

    imports and renders Logout in route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // COMPONENTS
 import Main from './components/main/Main.js';
 import Login from './components/main/Login.js';
+import Logout from './components/main/Logout.js;'
 
 // ACTIONS
 import { connect } from 'react-redux';
@@ -22,6 +23,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/logout" element={<Logout />} />
         </Routes>
       </div>
     </Router>
